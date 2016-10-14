af1800d6c3cd267b9cdb597ca8d1ab07ed6a665f
Mohammed Hawari <mohammed@hawari.fr>
2016-10-14 16:54:32 +0200

    MacOS Keychain integration working

--- lib/kitchen/driver/scalr_cred.rb
+++ lib/kitchen/driver/scalr_cred.rb
@@ -40,15 +40,16 @@ module Kitchen
           credentialsServiceName = "kitchen_scalr.cred"
           #First, we detect if there is any password in the keychain
           res = `security find-generic-password -l #{credentialsServiceName} -g >/dev/null 2>/dev/null; echo $?`
+          res = res.chomp
           if res == '0' then
-            account = `security find-generic-password -l #{credentialsServiceName} | grep acct | awk -F"=" '{print $2}' | sed 's/"//g'`
-            password = `security find-generic-password -l #{credentialsServiceName} -g 2>&1 | grep password | awk '{print $2}' | sed 's/"//g'`
+            account = `security find-generic-password -l #{credentialsServiceName} | grep acct | awk -F"=" '{print $2}' | sed 's/"//g'`.chomp
+            password = `security find-generic-password -l #{credentialsServiceName} -g 2>&1 | grep password | awk '{print $2}' | sed 's/"//g'`.chomp
             config[:scalr_api_key_id] = account
             config[:scalr_api_key_secret] = password
           else
             #Prompt for credentials
             print 'Enter your API Key ID: '
-            apiKeyId = gets.chomp
+            apiKeyId = STDIN.gets.chomp
             print 'Enter you API Key secret: '
             apiKeySecret = STDIN.noecho(&:gets).chomp
             #Save those in Keychain
